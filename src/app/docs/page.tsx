b568e30763724585777b3d4e667d68565070c641
Ben Reinhart <benjreinhart@gmail.com>
2023-07-31 18:58:25 -0700

    Add file upload component

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { FileForm } from "@/components/file-input";
+
 export default function Docs() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Docs</h1>
+    <main className="flex flex-col items-center justify-between p-24">
+      <div>
+        <FileForm
+          label="PDF"
+          accept={["application/pdf"]}
+          onSubmit={(files) =>
+            console.log("Chat with the following pdfs", files)
+          }
+        />
+      </div>
     </main>
   );
 }
---
+++ src/components/file-input.tsx
@@ -0,0 +1,66 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export const CONTENT_TYPE_TO_EXT_MAP = {
+  "text/csv": "csv",
+  "application/pdf": "pdf",
+  "text/plain": "text",
+} as const;
+
+export type ContentType = keyof typeof CONTENT_TYPE_TO_EXT_MAP;
+
+type PropsType = {
+  label: string;
+  accept?: ContentType[];
+  onSubmit: (files: File[]) => void;
+};
+
+export function FileForm(props: PropsType) {
+  const { label, accept, onSubmit } = props;
+
+  const [files, setFiles] = useState<File[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const files = e.target.files;
+
+    if (!files) {
+      return;
+    }
+
+    const file = files[0];
+    const type = file.type as ContentType;
+
+    if (Array.isArray(accept) && !accept.includes(type)) {
+      const fileTypes = accept.map((t) => CONTENT_TYPE_TO_EXT_MAP[t]);
+      setError(`Supported file types: ${fileTypes.join(", ")}`);
+    } else {
+      setError(null);
+      setFiles([file]);
+    }
+  }
+
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="file-input">{label}</Label>
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input
+          id="file-input"
+          type="file"
+          onChange={onChange}
+          className={`cursor-pointer${error ? " border-red-700" : ""}`}
+        />
+        <Button
+          type="submit"
+          disabled={files.length === 0}
+          onClick={() => onSubmit(files)}
+        >
+          Continue
+        </Button>
+      </div>
+      {error && <span className="text-red-700 text-sm">{error}</span>}
+    </div>
+  );
+}
