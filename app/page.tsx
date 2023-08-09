efc50d619a8aa83428041a45bfed9e4eca2ac48d
Ben Reinhart <benjreinhart@gmail.com>
2023-08-08 20:58:32 -0700

    New layout for guided tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -44,16 +44,52 @@ const Uploader = () => {
   };
 
   return (
-    <div className="mt-24 flex items-center justify-center w-full max-w-3xl">
-      <PDFUploadForm onSubmit={handleSubmit} uploading={uploading} />
+    <div className="flex items-center justify-center w-full">
+      <div className="py-2 w-[500px]">
+        <PDFUploadForm onSubmit={handleSubmit} uploading={uploading} />
+      </div>
     </div>
   );
 };
 
 export default function LandingPage() {
   return (
-    <main className="h-screen flex flex-col items-center">
-      <Uploader />
+    <main className="h-screen flex">
+      <section className="bg-muted bg-zinc-900 dark:border-r flex-[45_45_0%] pt-[52px]">
+        <div className="pl-6 pr-8 pt-24">
+          <h1 className="text-3xl lg:text-5xl font-bold leading-tight tracking-tighter">Welcome</h1>
+
+          <p className="mt-6">
+            This is a guided tutorial of Retrieval Augmented Generation (RAG) using the{' '}
+            <a
+              href="https://github.com/axilla-io/ax"
+              className="font-medium underline underline-offset-4"
+            >
+              ax framework
+            </a>
+            .
+          </p>
+
+          <p className="mt-6 border-l-[6px] pl-4 border-zinc-800 text-muted-foreground text-sm">
+            Retrieval Augmented Generation is a method that combines the strengths of large language
+            models and external retrieval or search mechanisms. In RAG, when generating a response,
+            the model first retrieves relevant information or documents from a large corpus and then
+            uses this retrieved information to conditionally generate an answer. This allows the
+            model to pull in external knowledge beyond its training data when answering questions.
+            <span className="block pt-4 font-semibold">ChatGPT</span>
+          </p>
+
+          <p className="mt-6">
+            For this tutorial, we're going to ask ChatGPT questions about our own documents using
+            RAG. Upload a PDF file less than 5MB to begin.
+          </p>
+        </div>
+      </section>
+      <section className="flex-[55_55_0%] pt-[52px]">
+        <div className="px-6 pt-24">
+          <Uploader />
+        </div>
+      </section>
     </main>
   );
 }
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,23 +4,25 @@ import { GitHubLogoIcon, TwitterLogoIcon } from '@radix-ui/react-icons';
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between border-b boder-zinc-600 px-6 py-2">
-      <Link href="https://axilla.io" target="_blank">
-        <Image src="/axilla-logo-text-white.png" height={30} width={90} alt="Axilla logo" />
-      </Link>
+    <div className="w-full absolute bg-transparent">
+      <div className="flex items-center justify-between px-6 py-2">
+        <Link href="https://axilla.io" target="_blank">
+          <Image src="/axilla-logo-text-white.png" height={30} width={90} alt="Axilla logo" />
+        </Link>
 
-      <div className="flex items-center">
-        <p className="text-sm font-mono px-2">made with ax</p>
-        <Link href="https://github.com/axilla-io/ax">
-          <p className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 py-2 w-9 px-0">
-            <GitHubLogoIcon />
-          </p>
-        </Link>
-        <Link href="https://twitter.com/axilla_io">
-          <p className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 py-2 w-9 px-0">
-            <TwitterLogoIcon />
-          </p>
-        </Link>
+        <div className="flex items-center">
+          <p className="text-sm font-mono px-2">made with ax</p>
+          <Link href="https://github.com/axilla-io/ax">
+            <p className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 py-2 w-9 px-0">
+              <GitHubLogoIcon />
+            </p>
+          </Link>
+          <Link href="https://twitter.com/axilla_io">
+            <p className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 py-2 w-9 px-0">
+              <TwitterLogoIcon />
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
   );
